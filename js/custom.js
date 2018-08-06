f99fe2131d40689ba99b60a4a3c59617d4a4360c
icao <clark012389@hotmail.com>
2018-08-06 14:28:02 -0500

    Structure refactoring

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    addOverlayEvents('presentation');
-    addOverlayEvents('financing');
-    addOverlayEvents('indices');
-    addOverlayEvents('methodology');
-    addOverlayEvents('references');
+  addOverlayEvents('presentation');
+  addOverlayEvents('financing');
+  addOverlayEvents('indices');
+  addOverlayEvents('methodology');
+  addOverlayEvents('references');
 });
 
 const addOverlayEvents = selector => {
-    document.querySelector(`.js-open-${selector}`).addEventListener('click', (e) => {
-        e.preventDefault();
-        
-        document.querySelector(`.overlay-${selector}`).style.display = 'block';
-    })
+  document.querySelector(`.js-open-${selector}`).addEventListener('click', (e) => {
+    e.preventDefault();
 
-    document.querySelector(`.js-close-overlay-${selector}`).addEventListener('click', (e) => {
-        e.preventDefault();
+    document.querySelector(`.overlay-${selector}`).style.display = 'block';
+  })
 
-        e.target.parentNode.parentNode.parentNode.style.display = 'none';
-    })
+  document.querySelector(`.js-close-overlay-${selector}`).addEventListener('click', (e) => {
+    e.preventDefault();
+
+    e.target.parentNode.parentNode.parentNode.style.display = 'none';
+  })
 }
